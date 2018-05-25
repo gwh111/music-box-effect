b403847b2393e5736a6293c4ae2a48eee939c1b8
gwh111 <173695508@qq.com>
2018-05-25 11:46:33 +0800

    init
    init

--- testmusicfadein/ViewController.m
+++ testmusicfadein/ViewController.m
@@ -7,23 +7,43 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import "MusicBox.h"
 
-@interface ViewController ()
+@interface ViewController ()<AVAudioPlayerDelegate>{
+    AVAudioPlayer *audioPlayer;
+}
 
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [MusicBox getInstance].fade=YES;
+    [MusicBox getInstance].replayTimes=2;
+    [[MusicBox getInstance]playMusic:@"战斗2" type:@"mp3"];
+    
+    double delayInSeconds = 3;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds *   NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        [[MusicBox getInstance]playEffect:@"女再见1" type:@"mp3"];
+    });
+    
+    {
+        double delayInSeconds = 6;
+        dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds *   NSEC_PER_SEC));
+        dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+            [[MusicBox getInstance]playMusic:@"周杰伦这所有的一切" type:@"mp3"];
+        });
+    }
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
